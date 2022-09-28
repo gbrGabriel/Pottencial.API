a1b732f329792f40ed3ccf3a65d33c3e267f5554
Gabriel <gabrielgbr.contato@hotmail.com>
2022-09-28 11:57:15 -0300

    Pasta de Contexto

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pottencial.API.Entities;
+
+namespace Pottencial.API.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options)
+        : base(options) { }
+
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
